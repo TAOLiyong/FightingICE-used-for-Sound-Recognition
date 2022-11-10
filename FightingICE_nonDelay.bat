1b2e18193a6e975f571b3f4a18d914f4d579bee5
胤月清 <85954604+TAOLiyong@users.noreply.github.com>
2022-11-10 16:20:42 +0900

    Add files via upload

---
+++ FightingICE_nonDelay.bat
@@ -0,0 +1,11 @@
+﻿::rem batファイルサンプル
+
+setlocal ENABLEDELAYEDEXPANSION
+::-Xmx8240m -Xms8240m -Xmn5120m 
+:: close CUDA code
+set CUDA_VISIBLE_DEVICES=100
+java -cp FightingICE_nonDelay.jar;./lib/lwjgl/*;./lib/natives/windows/*;./lib/*;  Main  --py4j --limithp 400 400
+rem TIMEOUT /T -1
+endlocal
+pause
+exit
